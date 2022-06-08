3b1188960b727214e5d5fcd9949787ef84024920
Theodore R Brandon <81046969+theobigdog@users.noreply.github.com>
2022-06-08 10:33:38 -0700

    Parties can now be displayed.  This should work for a newly formed party, or a loaded one.

--- slayer_main.py
+++ slayer_main.py
@@ -1,6 +1,7 @@
 Slayer_Version = 3
 
 import flask as Flask
+import pandas as pd
 
 app = Flask.Flask(__name__, static_url_path = '', template_folder = 'templates')
 
@@ -9,7 +10,7 @@ num_opening_pages = 3
 @app.errorhandler(404)
 def page_not_found(e):
     print(str(e))
-    return Flask.render_template("error_page.html", msg = str(e))  # Thid redirects back to root
+    return Flask.render_template("error_page.html", msg = str(e))  # This redirects back to root
 
 @app.route("/")
 def get_home_page():
@@ -19,11 +20,23 @@ def get_home_page():
 def narrative(page):
     return Flask.render_template("narrative" + str(page) + ".html", previous_page = str(page - 1), next_page = str(page + 1), has_previous_page = page > 1, has_next_page = page < num_opening_pages)
 
+@app.route("/party_choice")
+def party_choice():
+    return Flask.render_template("party_choice.html")
+
 @app.route("/party_generation")
 def party_generation():
-    return Flask.render_template("party_generation.html", party = my_list)
+    return Flask.render_template("party_generation.html", party = mock_party)
 
-my_list = [
+@app.route("/get_party")
+def get_party():
+    return Flask.render_template("get_party.html")
+
+@app.route("/display_party/")
+def display_party():
+    return Flask.render_template("display_party.html", party = load_party_json)
+
+mock_party = [
     {
         'name' : "Bubba",
         'class' : 'something'
@@ -33,5 +46,10 @@ my_list = [
         'class' : 'Savior'
     }
 ]
+
+load_party_json = pd.read_json('saves/Ruxpin.json')
+
+# print(load_party_json)
+# input()
 
 app.run()
