12413ac05b6627c8da7e0f6a2b96bf79e445d08f
Theodore R Brandon <81046969+theobigdog@users.noreply.github.com>
2022-06-11 15:00:06 -0700

    Updated how saves and loads are done and improvements to css

--- slayer_server/party_management.py
+++ slayer_server/party_management.py
@@ -1,6 +1,6 @@
 
 from logging import NullHandler
-from flask import  render_template, request, redirect
+from flask import  render_template, request, redirect, flash
 from .slayer_app import SlayerApp
 import pandas as pd
 from os.path import exists
@@ -8,6 +8,12 @@ from os.path import exists
 print('Initializing party management ...')
 
 # hero_classes = ['Warrior', 'Shifter', 'Battle-Mage', 'Hunter', 'Wizard', 'Sapper', 'Cleric']
+
+def get_save_path(filename):
+    path = 'saves/' + filename
+    if not path.endswith('.json'):
+        path = path + '.json'
+    return path
 
 @SlayerApp.route("/party_choice")
 def party_choice():
@@ -24,30 +30,33 @@ def get_party(message):
 
 @SlayerApp.route("/display_party/", methods=['POST'])
 def display_party():
-    filename = 'saves/' + request.form['filename']
-    file_exists = exists(filename)
+    savename = get_save_path(request.form['filename'])
+    file_exists = exists(savename)
     if file_exists:
-        party = pd.read_json('saves/' + request.form['filename']) #load_party_json
-        return render_template("display_party.html", party = party)
+        party = pd.read_json(savename)
+        return render_template("display_party.html", party = party, filename = request.form['filename'])
     else:
         return redirect('/get_party/File%20Not%20Found')
 
-@SlayerApp.route("/query_update_names")
+@SlayerApp.route("/query_update_names", methods=['POST'])
 def query_update_names():
-    return render_template("query_update_names.html")
+    return render_template("query_update_names.html", filename= request.form["filename"])
 
-@SlayerApp.route("/save_game1")
+@SlayerApp.route("/save_game1", methods=['POST'])
 def save_game1():
-    return render_template("save_game1.html")
+    return render_template("save_game1.html", filename= request.form["filename"])
 
 @SlayerApp.route("/save_game2/", methods=['POST'])
 def save_game2():
-    save_name = request.form['save_name']
-    if not save_name.endswith('.json'):
-        save_name += '.json'
-    temp = pd.read_json('saves/test.json') ### This needs to be fixed so proper file is loaded
-    temp.to_json('saves/' + save_name) ### This needs to be fixed so proper file is saved
-    return render_template("save_game2.html")
+    save_name = get_save_path(request.form['save_name'])
+    file_exists = exists(save_name)
+    if file_exists:
+        return render_template("confirm_name.html", save_name = request.form['save_name'])
+    load_name = get_save_path(request.form['filename'])
+    temp = pd.read_json(load_name) ### This needs to be fixed so proper file is loaded
+    temp.to_json(save_name) ### This needs to be fixed so proper file is saved
+    flash("Your game has been saved.  Please record for future retrieval.  Or don't.  I don't really care.")
+    return render_template("query_update_names.html")
 
 @SlayerApp.route("/update_names")
 def update_names():
--- slayer_server/slayer_app.py
+++ slayer_server/slayer_app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, session, redirect, url_for, escape, render_template
+from flask import Flask, session, redirect, url_for, escape, render_template, flash
 # from flask_session import Session
 
 print('Initializing slayer app...')
@@ -8,8 +8,11 @@ Slayer_Version = 3
 
 SlayerApp = Flask(__name__, static_url_path = '', static_folder = '../static_html', template_folder = '../templates')
 
+SlayerApp.secret_key = 'somesortoftext'
+
 @SlayerApp.route("/")
 def get_home_page():
+    flash('Hello!  Prepare to Die!')
     return render_template('start_page.html', version = Slayer_Version)
 
 @SlayerApp.errorhandler(404)
